76019ba876bc898133fe1c472fe6d90d206c6d53
imangolf1313 <olopez13mx@gmail.com>
2024-06-18 09:48:31 -0600

    Compartir pdf del programa por dropbox

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -40,7 +40,7 @@
         <v-card-title>Programa</v-card-title>
         <v-card-text>
           <div style="text-align: center;">
-            <a href="https://drive.google.com/file/d/1g1Ns2rgnGAWJplIBUvSuU1yQI6l7px7u/view?usp=sharing" target="_blank" style="width: auto; height: 240px; position: relative; display: inline-block;">
+            <a href="https://www.dropbox.com/scl/fi/y8hu221u25wr8jsws33zr/PROGRAMA-PARA-LA-FORMACI-N-Y-DESARROLLO-PROFESIONAL-PFDP.pdf?rlkey=0bd1r5mqiiextcrrkeyysaljk&st=d0bcuert&dl=0" target="_blank" style="width: auto; height: 240px; position: relative; display: inline-block;">
               <img src="https://p6aqvvqp5i.execute-api.us-east-2.amazonaws.com/images/cover/386097/875945" style="height: 100%;">
             </a>
           </div>
